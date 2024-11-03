2be31a3587da66b3d3ab9544cf64e52fd2d92f60
Салават Садриев <mrhumidum@MacBook-Air-Salavat.local>
2024-11-03 17:28:04 +0300

    :)

--- Homework_1/obj/Debug/net8.0/Homework_1.AssemblyInfo.cs
+++ Homework_1/obj/Debug/net8.0/Homework_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97594d67bb63a770937f3b28537aef304018bee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
