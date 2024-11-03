a446c3ddc3a419af86bef33e68bb36b73d3621b3
MrHumidum <salavatcool5@gmail.com>
2024-11-03 17:45:08 +0300

    =(

--- Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
+++ Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97594d67bb63a770937f3b28537aef304018bee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be31a3587da66b3d3ab9544cf64e52fd2d92f60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
